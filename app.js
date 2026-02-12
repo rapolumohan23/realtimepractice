6d0eda709922e1254d56f498dd66216d7ff56a9a
rapolumohan23 <rapolumohan23@gmail.com>
2026-02-12 15:12:32 +0530

    Add additional console log statements

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 console.log("Hello, World!");
+console.log("Hello, My name is mohan");
+console.log("I hava total 7 years of exp");
+console.log("currently working with cognizant");
+console.log("Thank you");
+
