d7b3e4cd029649d8a5047d977367ea720dbceb42
rapolumohan23 <rapolumohan23@gmail.com>
2026-05-08 15:04:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ const server = http.createServer((req, res) => {
     res.end();
 });
 
-server.listen(3000, () => {
-    console.log("Server running on port 3000");
+server.listen(4000, () => {
+    console.log("Server running on port 4000");
 });
