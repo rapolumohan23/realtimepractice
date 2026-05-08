14fb96b0c02b75e4c3e3a5b9aaa760bd6cc6c630
rapolumohan23 <rapolumohan23@gmail.com>
2026-05-08 15:11:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.write("Hello, This is Mohan");
+    res.write("Hello from Node.js Docker App");
     res.end();
 });
 
